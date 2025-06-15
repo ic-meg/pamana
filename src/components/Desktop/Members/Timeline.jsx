574c9ca042d18ab0e379e477b5b394ef3a79aeeb
Daniela-FLC <danielafaithcinco@gmail.com>
2025-06-15 21:28:54 +0800

    timeline update

--- src/components/Desktop/Members/Timeline.jsx
+++ src/components/Desktop/Members/Timeline.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { Images, RizalTimelineImg } from "../../../assets"
+import React, { useEffect, useState, useCallback } from "react"
+import { Icons, Images, RizalTimelineImg } from "../../../assets"
+import Feather from "../Feather"
 
 export default function Timeline() {
   const [rotation, setRotation] = useState(0)
@@ -9,7 +10,7 @@ export default function Timeline() {
 
   const rizalTimeline = [
     {
-      date: "Jun 19, 1861",
+      date: "Jun 19, <br/>1861",
       event: "Birth of José Rizal",
       description:
         "Born in Calamba, Laguna, to Francisco Mercado and Teodora Alonso Realonda.",
@@ -32,14 +33,14 @@ export default function Timeline() {
     },
     {
       date: "1878",
-      event: "Enrolled in UST",
+      event: "Enrolled in <br/> UST",
       description:
         "Studied medicine and philosophy at the University of Santo Tomas.",
       image:
         "https://varsitarian.net/wp-content/uploads/2009/07/CXd_vHiUoAA7KTu-1.jpg",
     },
     {
-      date: "May 03, 1882",
+      date: "May 03, <br/>1882",
       event: "Departure to Spain",
       description:
         "Left for Europe to continue his studies and escape discrimination.",
@@ -72,7 +73,7 @@ export default function Timeline() {
     },
     {
       date: "Feb 1888",
-      event: "Exiled again",
+      event: "Exiled <br/>again",
       description:
         "Left the Philippines due to continuous harassment and threats.",
       image:
@@ -86,22 +87,22 @@ export default function Timeline() {
       image: "https://kahimyang.com/resources/el-filibusterismo-og.jpg",
     },
     {
-      date: "Jun 26, 1892",
+      date: "Jun 26, <br/>1892",
       event: "Returned to the Philippines",
       description: "Came back to initiate peaceful reforms.",
       image:
         "https://dimasalanglaonglaan.wordpress.com/wp-content/uploads/2014/07/download-1.jpg?w=680",
     },
     {
-      date: "Jul 03, 1892",
+      date: "Jul 03, <br/>1892",
       event: "Founded La Liga Filipina",
       description: "Established a civic organization for peaceful reforms.",
       image:
         "https://kahimyang.com/resources-7/rizal-la-liga-filipina-rizal.jpg",
     },
     {
-      date: "Jul 07, 1892",
-      event: "Exiled to Dapitan",
+      date: "Jul 07, <br/>1892",
+      event: "Exiled to <br/> Dapitan",
       description: "Arrested and exiled to Dapitan, Mindanao.",
       image:
         "https://www.vigattintourism.com/assets/article_main_photos/optimize/1347439535V3MjuntO.jpg",
@@ -115,7 +116,7 @@ export default function Timeline() {
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ-tYxbbaqZKRU79GsF5nAHekjPGOQmRR_tg&s",
     },
     {
-      date: "Oct 06, 1896",
+      date: "Oct 06, <br/>1896",
       event: "Arrested and imprisoned",
       description:
         "Captured on his way to Cuba and imprisoned at Fort Santiago.",
@@ -123,14 +124,14 @@ export default function Timeline() {
         "https://xiaochua.net/wp-content/uploads/2012/12/05-630-ng-umaga-nang-simulan-ni-rizal-at-ng-mga-kasama-ang-paglakad-patungo-sa-luneta-de-bagumabayan.jpg?w=584",
     },
     {
-      date: "Dec 26, 1896",
+      date: "Dec 26, <br/>1896",
       event: "Sentenced to death",
       description:
         "Tried and found guilty of rebellion, sedition, and conspiracy.",
       image: "https://kahimyang.com/resources-13/the-trial-of-jose-rizal.jpg",
     },
     {
-      date: "Dec 30, 1896",
+      date: "Dec 30, <br/>1896",
       event: "Execution at Bagumbayan",
       description:
         "Executed by firing squad; became a martyr of the Philippine revolution.",
@@ -145,153 +146,155 @@ export default function Timeline() {
 
   const rotateToIndex = useCallback(
     (index) => {
-      const anglePerItem = 360 / rizalTimeline.length;
-      const newRotation = -index * anglePerItem;
+      const anglePerItem = 360 / rizalTimeline.length
+      const newRotation = -index * anglePerItem
 
-      setActiveIndex(index);
+      setActiveIndex(index)
 
       if (isZoomed) {
-        setRotation(0);
-        setHandRotation(index * anglePerItem);
+        setRotation(0)
+        setHandRotation(index * anglePerItem)
       } else {
-        setRotation(newRotation);
+        setRotation(newRotation)
       }
     },
     [isZoomed, rizalTimeline.length]
-  );
-
+  )
 
   useEffect(() => {
     setHandRotation(0)
     rotateToIndex(activeIndex)
   }, [isZoomed, activeIndex, rotateToIndex])
 
-
   return (
-    <div className="relative w-full h-full overflow-hidden">
-      <div
-        className="absolute inset-0 bg-center bg-cover blur-sm brightness-75 z-0"
-        style={{
-          backgroundImage: `url("${
-            RizalTimelineImg[activeIndex] || Images.rizalBirth
-          }")`,
-        }}
-      ></div>
+    <Feather>
       <div className="relative w-full h-full">
-        <img
-          src={Images.oldpaper}
-          alt="Rizal Birth"
-          className="absolute right-0 top-5"
-          onError={(e) => {
-            e.target.onerror = null
-            e.target.src = Images.rizalBirth
-          }}
-        />
-
-        <img
-          src={RizalTimelineImg[activeIndex] || Images.rizalBirth}
-          alt={rizalTimeline[activeIndex]?.event || "Rizal"}
-          style={{
-            width: "50%",
-            height: "70%",
-            WebkitMaskImage: `url(${Images.mask})`,
-            WebkitMaskRepeat: "no-repeat",
-            WebkitMaskSize: "100% 120%",
-            WebkitMaskPosition: "center -3em",
-            maskImage: `url(${Images.mask})`,
-            maskRepeat: "no-repeat",
-            maskSize: "100% 120%",
-            maskPosition: "center -2.5em",
-          }}
-        />
-
-        <div className="absolute top-[7em] right-[3em] w-[48%] flex flex-col gap-3">
-          <h1 className="font-bold text-3xl ">
-            {rizalTimeline[activeIndex].event}
-          </h1>
-          <p className="ml-3 text-xl">
-            {rizalTimeline[activeIndex].description}
-          </p>
-        </div>
-      </div>
-
-      <div
-        className={`absolute transition-all duration-500 ease-in-out cursor-pointer ${
-          isZoomed
-            ? "left-1/2 translate-x-[-50%] bottom-[50%] translate-y-[50%] scale-110 z-50"
-            : "left-1/2 translate-x-[-50%] bottom-0 translate-y-[50%] scale-100"
-        }`}
-        onClick={() => setIsZoomed((prev) => !prev)}
-      >
-        <div className="relative w-[600px] h-[600px]">
-          {/* Rotating Timeline */}
-          <div
-            className="absolute w-full h-full transition-transform duration-700 ease-in-out"
-            style={{
-              transform: `rotate(${rotation}deg)`,
-            }}
-          >
-            {rizalTimeline.map((timeline, index) => {
-              const angle =
-                (index / rizalTimeline.length) * 2 * Math.PI - Math.PI / 2
-
-              const x = centerX + radius * Math.cos(angle) - 40
-              const y = centerY + radius * Math.sin(angle) - 10
-
-              return (
-                <button
-                  key={index}
-                  title={timeline.event}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    rotateToIndex(index);
-                  }}
-                  className={`absolute text-xs font-medium cursor-pointer hover:underline text-center w-[80px] text-3xl ${
-                    index === activeIndex ? "text-black-600" : "text-white"
-                  }`}
-                  style={{
-                    left: `${x}px`,
-                    top: `${y}px`,
-                    transform: `rotate(${-rotation}deg)`,
-                    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.8)",
-                    fontWeight: "bolder",
-                    background: "none",
-                    border: "none",
-                  }}
-                >
-                  {timeline.date}
-                </button>
-              );
-            })}
+        <div className="relative w-[90%] flex flex-col m-2  mr-5 ml-auto gap-10">
+          <div className="w-full flex flex-col gap-3 mb-1 pl-10 h-min-[600px]">
+            <h1
+              className="text-[36px] sm:text-[55px] font-extrabold uppercase tracking-wide leading-none font-coustard pl-24 w-full break-words"
+              dangerouslySetInnerHTML={{
+                __html: rizalTimeline[activeIndex].event,
+              }}
+            />
+            <div className="flex gap-3">
+              <img src={Icons.oldclock} alt="Rizal Birth" className="h-10" />
+              <h2 className="text-xl sm:text-2xl mt-2 font-bold tracking-wider font-coustard">
+                {rizalTimeline[activeIndex].date.replace(/<[^>]+>/g, "")}
+              </h2>
+            </div>
           </div>
 
-          {/* Clock Body */}
-          <img
-            className="absolute top-1/2 left-1/2 w-[350px] h-[350px] -translate-x-1/2 -translate-y-1/2 -z-10"
-            src={Images.clockBody}
-            alt="Clock Body"
-            draggable={false}
-          />
+          <p className="-mt-5  text-[1px] sm:text-[17px] leading-relaxed text-left max-w-[50%] min-h-14">
+            {rizalTimeline[activeIndex].description}
+          </p>
 
-          {/* Clock Hand */}
           <img
-            className="absolute top-1/2 left-1/2 w-[350px] h-[350px] -translate-x-1/2 -translate-y-1/2 -z-10 rotate-45"
-            src={Images.clockHandHour}
-            alt="Clock Hand"
-            draggable={false}
-          />
-          <img
-            className="absolute top-1/2 left-1/2 w-[350px] h-[350px] -z-10"
-            src={Images.clockHandMinute}
-            alt="Clock Hand"
-            draggable={false}
+            src={RizalTimelineImg[activeIndex] || Images.rizalBirth}
+            alt={rizalTimeline[activeIndex]?.event || "Rizal"}
             style={{
-              transform: `translate(-50%, -50%) rotate(${handRotation}deg)`,
-              transformOrigin: "50% 50%",
+              position: "absolute",
+              top: 40,
+              right: -20,
+              zIndex: -10,
+              width: 450,
+              height: 300,
+              WebkitMaskImage: `url(${Images.mask})`,
+              WebkitMaskRepeat: "no-repeat",
+              WebkitMaskSize: "100% 120%",
+              WebkitMaskPosition: "center -3em",
+              maskImage: `url(${Images.mask})`,
+              maskRepeat: "no-repeat",
+              maskSize: "100% 120%",
+              maskPosition: "center -2.5em",
             }}
           />
         </div>
+        {isZoomed && (
+          <div
+            className="fixed inset-0 z-40 bg-black/30 backdrop-blur-sm transition-opacity duration-500 mt-[37px]"
+            onClick={() => setIsZoomed((prev) => !prev)}
+          />
+        )}
+        <div
+          className={`absolute transition-all duration-500 ease-in-out cursor-pointer ${
+            isZoomed
+              ? "left-1/2 translate-x-[-50%] bottom-[50%] translate-y-[66%] scale-110 z-50"
+              : "left-1/2 translate-x-[-50%] bottom-0 translate-y-[90%] scale-100"
+          }`}
+          onClick={() => setIsZoomed((prev) => !prev)}
+        >
+          <div className="relative w-[600px] h-[600px]">
+            {/* Rotating Timeline */}
+            <div
+              className="absolute w-full h-full transition-transform duration-700 ease-in-out"
+              style={{
+                transform: `rotate(${rotation}deg)`,
+              }}
+            >
+              {rizalTimeline.map((timeline, index) => {
+                const angle =
+                  (index / rizalTimeline.length) * 2 * Math.PI - Math.PI / 2
+
+                const x = centerX + radius * Math.cos(angle) - 40
+                const y = centerY + radius * Math.sin(angle) - 10
+
+                return (
+                  <button
+                    key={index}
+                    title={timeline.event}
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      rotateToIndex(index)
+                    }}
+                    className={`absolute text-xs font-medium cursor-pointer hover:underline text-center w-[80px] text-3xl ${
+                      index === activeIndex ? "text-[#764123]" : "text-white"
+                    }`}
+                    style={{
+                      left: `${x}px`,
+                      top: `${y}px`,
+                      transform: `rotate(${-rotation}deg)`,
+                      textShadow: "2px 2px 4px rgba(0, 0, 0, 0.8)",
+                      fontWeight: "bolder",
+                      background: "none",
+                      border: "none",
+                    }}
+                    dangerouslySetInnerHTML={{
+                      __html: timeline.date,
+                    }}
+                  />
+                )
+              })}
+            </div>
+
+            {/* Clock Body */}
+            <img
+              className="absolute top-1/2 left-1/2 w-[350px] h-[350px] -translate-x-1/2 -translate-y-1/2 -z-10"
+              src={Images.clockBody}
+              alt="Clock Body"
+              draggable={false}
+            />
+
+            {/* Clock Hand */}
+            <img
+              className="absolute top-1/2 left-1/2 w-[350px] h-[350px] -translate-x-1/2 -translate-y-1/2 -z-10 rotate-45"
+              src={Images.clockHandHour}
+              alt="Clock Hand"
+              draggable={false}
+            />
+            <img
+              className="absolute top-1/2 left-1/2 w-[350px] h-[350px] -z-10"
+              src={Images.clockHandMinute}
+              alt="Clock Hand"
+              draggable={false}
+              style={{
+                transform: `translate(-50%, -50%) rotate(${handRotation}deg)`,
+                transformOrigin: "50% 50%",
+              }}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </Feather>
   )
 }
